2b7ba8ccc9762e5d9f884bd481db75ea2f6792c1
satyajit-zecdata <satyajit@zecdata>
2023-05-14 22:10:45 +0530

    adding project to the github, added Nextauth and authentication

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import ToasterContext from './context/ToasterContext'
+import AuthContext from './context/AuthContext'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Messenger App',
+  description: 'Created by Satyajit Giram',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext/>
+          {children}
+        </AuthContext>
+        </body>
+    </html>
+  )
+}
